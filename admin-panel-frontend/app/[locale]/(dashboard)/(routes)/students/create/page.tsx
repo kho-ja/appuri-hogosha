de78e56761a2e9f6a39edef712c2a2e1ba9b8515
Soliha Ismoilova <88421636+Ismoilova1031@users.noreply.github.com>
2025-04-19 18:39:38 +0500

    作成 保存ga o'zgartirildi (#102)
    * 作成 保存ga o'zgartirildi
    
    * qolib ketgan 保存 lar to'g'irlab qo'yildi
    
    * barcha tillarda to'g'irlab chiqildi
    
    * groupda ham 保存 bo'lib ketib qolgan ekan 作成 qilib qo'ydim
    
    * o'zbek tili ham to'g'irlandi
    
    * guruh uchun ham button o'zgartirildi

--- admin-panel-frontend/app/[locale]/(dashboard)/(routes)/students/create/page.tsx
+++ admin-panel-frontend/app/[locale]/(dashboard)/(routes)/students/create/page.tsx
@@ -218,7 +218,7 @@ export default function CreateStudent() {
           </FormItem>
 
           <Button disabled={isPending}>
-            {t("Submit") + (isPending ? "..." : "")}
+            {t("CreateStudent") + (isPending ? "..." : "")}
           </Button>
         </form>
       </Form>
