e163194312684042851bca7fff2c504036210e12
Asadbek Rabbimmov <152362117+Raozas@users.noreply.github.com>
2025-05-10 18:55:06 +0900

    157 translations and login crash issue (#158)
    * enhance sign-out process and improve loading state in MessageList
    
    * add text size and theme mode translations to settings

--- mobile-frontend/app/(app)/(tabs)/(home)/index.tsx
+++ mobile-frontend/app/(app)/(tabs)/(home)/index.tsx
@@ -4,7 +4,7 @@ import { useStudents } from '@/contexts/student-context'; // Your student contex
 import { ThemedText } from '@/components/ThemedText';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { StudentSelector } from '@/components/StudentSelector'; // Your selector component
-import MessageList from '@/components/MessageList';
+// import MessageList from '@/components/MessageList';
 import { useTheme } from '@rneui/themed';
 import { useFocusEffect } from '@react-navigation/native';
 import { useCallback } from 'react';
@@ -44,9 +44,9 @@ const HomeScreen = () => {
   }
 
   // If there's only one student, show their messages directly
-  if (students.length === 1) {
-    return <MessageList studentId={students[0].id} />;
-  }
+  // if (students.length === 1) {
+  //   return <MessageList studentId={students[0].id} />;
+  // }
 
   // If there are multiple students, show the selection list
   return (
