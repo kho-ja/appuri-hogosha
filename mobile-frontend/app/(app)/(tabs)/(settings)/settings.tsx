6d06abb3b6050736e91ad3101d997baac97887b0
jdu211109 <211109m@jdu.uz>
2025-06-17 00:53:28 +0500

    feat: add changes

--- mobile-frontend/app/(app)/(tabs)/(settings)/settings.tsx
+++ mobile-frontend/app/(app)/(tabs)/(settings)/settings.tsx
@@ -212,6 +212,21 @@ export default function SettingsScreen() {
             snapPoints={snapPoints}
             backgroundStyle={{ backgroundColor: '#eee' }}
             onDismiss={() => setIsOpen(false)}
+            backdropComponent={() => (
+              <Pressable
+                style={{
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                }}
+                onPress={() => {
+                  // @ts-ignore
+                  bottomSheetModalRef.current?.dismiss();
+                }}
+              />
+            )}
           >
             <ThemedView style={styles.contentContainer}>
               <ThemedView style={styles.row}></ThemedView>
