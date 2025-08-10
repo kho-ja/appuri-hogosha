aa7876e86bfe57c6bf16749e93cd8e0d6ae0eb94
TheApro <webanonym2005@gmail.com>
2025-08-10 21:35:36 +0500

    Increase email max length to 254 as per RFC recommendation

--- admin-panel-frontend/app/[locale]/(dashboard)/(routes)/students/create/page.tsx
+++ admin-panel-frontend/app/[locale]/(dashboard)/(routes)/students/create/page.tsx
@@ -33,7 +33,7 @@ const GetFormSchema = (t: (key: string) => string) => {
      email: z
       .string()
       .email()
-      .max(64, { message: t("Email is too long") }),
+      .max(254, { message: t("Email is too long") }),
     phone_number: z
       .string()
       .min(10)
