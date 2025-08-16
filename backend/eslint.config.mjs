5c48e03d0f22f154f49c84d5377b921df0b2fcf3
TheApro <webanonym2005@gmail.com>
2025-08-17 01:51:52 +0500

    Setup completed

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,28 @@
+import pluginPrettier from 'eslint-plugin-prettier';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default {
+  files: ['**/*.{js,cjs,mjs,ts,tsx}'],
+  languageOptions: {
+    parser: tsParser,
+    parserOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+    },
+    globals: {
+      process: 'readonly',
+      __dirname: 'readonly',
+      require: 'readonly',
+    },
+  },
+  plugins: {
+    prettier: pluginPrettier,
+    '@typescript-eslint': tsPlugin,
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+  },
+};
