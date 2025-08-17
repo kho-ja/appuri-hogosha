d738466343d561b879e3b6c115ab38e115d41c48
TheApro <webanonym2005@gmail.com>
2025-08-17 15:50:27 +0500

    Fixed one more

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -23,9 +23,7 @@ export default [
     },
     rules: {
       'prettier/prettier': 'error',
-
       'no-unused-vars': 'off',
-
       '@typescript-eslint/no-unused-vars': [
         'warn',
         { argsIgnorePattern: '^_' },
