e207c51eb58a2d6f654f4f63ca1450c058359a78
SunnatillaSobitjonov <sunnatillasobitjonov81@gmail.com>
2025-08-24 01:36:12 +0500

    Parent Notification web pagesi dizayini o'zgartirildi

--- admin-panel-frontend/app/[locale]/(public)/parentnotification/page.tsx
+++ admin-panel-frontend/app/[locale]/(public)/parentnotification/page.tsx
@@ -7,9 +7,10 @@ import localImageLoader from "@/lib/localImageLoader";
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-blue-50 to-white dark:from-slate-950 dark:to-slate-900 dark:text-white">
-      <header className="container flex items-center justify-between pt-4">
+      <header className="sticky top-0 z-50 bg-white/70 dark:bg-slate-900/70 backdrop-blur-md border-b border-gray-200 dark:border-slate-800">
+      <div className="container flex items-center justify-between py-4">
         <div className="flex items-center gap-2">
-          <span className="text-3xl font-bold tracking-tighter">
+          <span className="text-2xl font-extrabold tracking-tight">
             Parent Notification
           </span>
         </div>
@@ -21,16 +22,17 @@ export default function Home() {
             Features
           </Link>
         </nav>
+      </div>
       </header>
 
       {/* Hero Section */}
-      <section className="container grid items-center gap-8 pb-8 pt-16 md:grid-cols-2 md:gap-16">
+      <section className="container grid items-center gap-8 pb-8 pt-20 md:grid-cols-2 md:gap-16">
         <div className="space-y-6">
           <div className="space-y-2">
             <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl">
               Stay Connected
               <br />
-              with Your Child&apos;s Education
+              with Your Child's Education
             </h1>
             <p className="text-gray-500 dark:text-gray-400">
               Real-time updates on university activities, grades, and
@@ -47,7 +49,7 @@ export default function Home() {
                 alt="Get it on Google Play"
                 width={646}
                 height={250}
-                className="h-12 w-auto"
+                className="h-12 w-auto hover:scale-105 transition"
                 priority
               />
             </Link>
@@ -58,35 +60,33 @@ export default function Home() {
                 alt="Download on the App Store"
                 width={646}
                 height={250}
-                className="h-12 w-auto"
+                className="h-12 w-auto hover:scale-105 transition"
                 priority
               />
             </Link>
           </div>
-          <div className="w-32">
             <p className="mt-2 text-sm text-gray-500 dark:text-gray-400">
               Scan to get the app
             </p>
           </div>
-        </div>
-        <div className="relative h-[500px] flex items-center justify-center">
+        <div className="relative flex items-center justify-center">
           <Image
             loader={localImageLoader}
-            src="/assets/notification-bell.svg"
+            src="/assets/undraw_fill_form_re_cwyf.svg"
             alt="Notification Bell"
-            width={256}
-            height={256}
-            className="animate-pulse"
+            width={300}
+            height={300}
+            className="animate-pulse hover:scale-105 transition"
             priority
           />
         </div>
       </section>
 
       {/* Features Section */}
-      <section className="container grid gap-8 py-16 md:grid-cols-2 md:gap-16">
-        <div className="space-y-4">
+      <section className="container grid gap-12 py-20 md:grid-cols-2 items-center">
+        <div className="space-y-6">
           <h2 className="text-3xl font-bold">Real-time Updates</h2>
-          <p className="text-gray-500 dark:text-gray-400">
+          <p className="text-gray-600 dark:text-gray-400">
             Get instant notifications about your child&apos;s attendance,
             grades, and university events. Stay informed about their academic
             progress and campus activities.
@@ -95,36 +95,36 @@ export default function Home() {
             Learn More About Features
           </Button>
         </div>
-        <div className="relative h-[500px] flex items-center justify-center">
+        <div className="relative flex items-center justify-center">
           <Image
             loader={localImageLoader}
             src="/assets/undraw_browsing_online_re_umsa.svg"
             alt="Calendar"
-            width={656}
-            height={656}
-            className="h-41 w-auto"
+            width={500}
+            height={500}
+            className="hover:scale-105 transition"
           />
         </div>
       </section>
 
       {/* Forms Section */}
-      <section className="container grid gap-8 py-16 md:grid-cols-2 md:gap-16">
-        <div className="relative h-[500px] order-last md:order-first flex items-center justify-center">
+      <section className="container grid gap-12 py-20 md:grid-cols-2 items-center">
+        <div className="relative order-last md:order-first flex items-center justify-center">
           <Image
             loader={localImageLoader}
             src="/assets/undraw_fill_form_re_cwyf.svg"
             alt="Calendar"
-            width={656}
-            height={656}
-            className="h-41 w-auto"
+            width={500}
+            height={500}
+            className="hover:scale-105 transition"
           />
         </div>
-        <div className="space-y-4">
-          <div className="text-sm font-medium dark:text-gray-300">
+        <div className="space-y-6">
+          <span className="text-sm uppercase tracking-wide text-gray-600 dark:text-gray-400">
             Easy Communication
-          </div>
+          </span>
           <h2 className="text-3xl font-bold">Submit Forms Digitally</h2>
-          <p className="text-gray-500 dark:text-gray-400">
+          <p className="text-gray-600 dark:text-gray-400">
             Easily submit absence forms and communicate with teachers through
             the app. No more paper forms or delayed communications.
           </p>
@@ -132,66 +132,65 @@ export default function Home() {
       </section>
 
       {/* Communication Section */}
-      <section className="container grid gap-8 py-16 md:grid-cols-2 md:gap-16">
-        <div className="space-y-4">
+      <section className="container grid gap-12 py-20 md:grid-cols-2 items-center">
+        <div className="space-y-6">
           <h2 className="text-3xl font-bold">
             Enhanced Parent-Teacher Connection
           </h2>
-          <p className="text-gray-500 dark:text-gray-400">
+          <p className="text-gray-600 dark:text-gray-400">
             Stay updated about parent-teacher meetings, school holidays, and
             important events. Direct communication channel with teachers and
             school administration.
           </p>
         </div>
-        <div className="relative h-[500px] flex items-center justify-center">
+        <div className="relative flex items-center justify-center">
           <Image
             loader={localImageLoader}
             src="/assets/undraw_undraw_undraw_undraw_smartphone_34c3_-1-_orrt_-1-_tyrp_-1-_fl8c.svg"
             alt="Calendar"
-            width={256}
-            height={256}
-            className="h-[500px] w-auto object-contain"
+            width={300}
+            height={300}
+            className="h-[400px] w-auto object-contain hover:scale-105 transition"
           />
         </div>
       </section>
 
       {/* Download Section */}
-      <section className="container space-y-8 py-16 text-center">
-        <h2 className="text-3xl font-bold">
+      <footer className="relative py-10 bg-gray-50 dark:bg-slate-800 text-center">
+      <div className="container space-y-6">
+        <h2 className="text-2xl font-bold">
           Download Parent Notification Today
         </h2>
-        <p className="text-gray-500 dark:text-gray-400">
+        <p className="max-w-xl mx-auto text-gray-600 dark:text-gray-400">
           Free to download and use. Stay connected with your child&apos;s
           education journey.
         </p>
         <div className="flex flex-wrap items-center justify-center gap-8">
-          <div className="w-32">{/* QR Code space */}</div>
-          <div className="flex flex-col gap-4">
-            <Link href="#">
+            <Link href="https://play.google.com/store/apps/details?id=com.jduapp.parentnotification">
               <Image
                 loader={localImageLoader}
                 src="/assets/google.png"
                 alt="Get it on Google Play"
                 width={646}
                 height={250}
-                className="h-12 w-auto"
+                className="h-12 w-auto hover:scale-110 transition"
                 priority
               />
             </Link>
-            <Link href="#">
+            <Link href="https://apps.apple.com/uz/app/parent-notification/id6744873338">
               <Image
                 loader={localImageLoader}
                 src="/assets/apple.png"
                 alt="Download on the App Store"
                 width={646}
                 height={250}
-                className="h-12 w-auto"
+                className="h-12 w-auto hover:scale-110 transition"
                 priority
               />
             </Link>
           </div>
         </div>
-      </section>
+      </footer>
     </div>
   );
 }
