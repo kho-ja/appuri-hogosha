bb21ebf41c14d00d8f4931c0ce5f0eec168fb3c8
TheApro <webanonym2005@gmail.com>
2025-08-23 13:32:31 +0500

    Login page encancement and new password component

--- admin-panel-frontend/app/[locale]/(auth)/login/page.tsx
+++ admin-panel-frontend/app/[locale]/(auth)/login/page.tsx
@@ -16,56 +16,57 @@ import { login } from "@/login";
 import LanguageSelect from "@/components/LanguageSelect";
 import { ToggleMode } from "@/components/toggle-mode";
 import { useToast } from "@/components/ui/use-toast";
-import { CircleCheckBig, CircleX } from "lucide-react";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMakeZodI18nMap } from "@/lib/zodIntl";
+import NewPasswordInput, {
+  validateNewPassword,
+} from "@/components/NewPasswordInput";
+
+const formSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(1),
+});
 
 export default function LoginForm() {
+  const zodErrors = useMakeZodI18nMap();
+  z.setErrorMap(zodErrors);
+
   const [newPasswordError, setNewPasswordError] = useState<string | null>(null);
   const [newPassword, setNewPassword] = useState<boolean>(false);
   const router = useRouter();
   const t = useTranslations("LoginForm");
   const { toast } = useToast();
-  const [isFocused, setIsFocused] = useState(false);
   const [feedbackPassword, setFeedbackPassword] = useState("");
 
-  const requirements = [
-    {
-      text: t("8-character minimum length"),
-      test: (pw: string) => pw.length >= 8,
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
     },
-    {
-      text: t("Contains at least 1 number"),
-      test: (pw: string) => /\d/.test(pw),
-    },
-    {
-      text: t("Contains at least 1 lowercase letter"),
-      test: (pw: string) => /[a-z]/.test(pw),
-    },
-    {
-      text: t("Contains at least 1 uppercase letter"),
-      test: (pw: string) => /[A-Z]/.test(pw),
-    },
-    {
-      text: t("Contains at least 1 special character"),
-      subText: `^ $ * . { } ( ) ? \" ! @ # % & / \\ > < ' : ; | _ ~ \` + = `,
-      test: (pw: string) => /[\^$*.[\]{}()?"!@#%&/\\><':;|_~`+=]/.test(pw),
-    },
-  ];
+  });
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
+  const handleSubmit = async (values: z.infer<typeof formSchema>) => {
     toast({
       title: t("LoggingIn"),
       description: t("wait"),
     });
 
-    const formData = new FormData(e.currentTarget);
-    const email = formData.get("email") as string;
-    const password = formData.get("password") as string;
-    const newPassword = (formData.get("newPassword") ?? "") as string;
+    const { email, password } = values;
+    const newPasswordValue = newPassword ? feedbackPassword : "";
 
     try {
-      const response = await login(email, password, newPassword);
+      const response = await login(email, password, newPasswordValue);
 
       if (typeof response === "string") {
         router.push("/");
@@ -116,97 +117,78 @@ export default function LoginForm() {
             <CardDescription>{t("description")}</CardDescription>
           </CardHeader>
           <CardContent>
-            <form onSubmit={handleSubmit} className="grid gap-4">
-              <div className="grid gap-2">
-                <Label htmlFor="email">{t("emailLabel")}</Label>
-                <Input
-                  id="email"
-                  type="email"
-                  name="email"
-                  placeholder={t("emailPlaceholder")}
-                  required
-                />
-              </div>
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">{t("passwordLabel")}</Label>
-                  <Link
-                    href="/forgot-password"
-                    className="ml-auto inline-block text-sm underline"
-                  >
-                    {t("forgotPassword")}
-                  </Link>
-                </div>
-                <Input id="password" type="password" name="password" required />
-                {newPasswordError && (
-                  <div className="text-red-500 text-sm">{newPasswordError}</div>
-                )}
-              </div>
-              {newPassword && (
-                <div className="grid gap-2">
-                  <div className="flex items-center">
-                    <Label htmlFor="newPassword">{t("newPasswordLabel")}</Label>
-                  </div>
-                  <div
-                    className="relative space-y-4"
-                    onFocus={() => setIsFocused(true)}
-                    onBlur={() => setIsFocused(false)}
-                  >
-                    <Input
-                      id="newPassword"
-                      type="password"
-                      name="newPassword"
-                      value={feedbackPassword}
-                      onChange={(e) => setFeedbackPassword(e.target.value)}
-                      required
-                    />
-                    <div className="text-red-500">{t("OTPError")}</div>
-                    {isFocused && (
-                      <div className="absolute left-0 bottom-[130%] translate-y-0 shadow-lg rounded-md border-border border-2 w-full text-foreground">
-                        <div className="relative z-50 bg-muted p-2 rounded-sm ">
-                          <div>{t("requirements")}</div>
-                          {requirements.map((req, index) => (
-                            <div key={index} className="flex items-start">
-                              {req.test(feedbackPassword) ? (
-                                <span className="text-green-500">
-                                  <CircleCheckBig className="w-4 h-4" />
-                                </span>
-                              ) : (
-                                <span className="text-red-500">
-                                  <CircleX className="w-4 h-4" />
-                                </span>
-                              )}
-                              <span className="ml-1 text-xs">
-                                {req.text} <br />{" "}
-                                {req.subText ? `${req.subText}` : ""}
-                              </span>
-                            </div>
-                          ))}
-                        </div>
-                        <div className="absolute right-6 p-[2px] rounded-sm -bottom-[10px] -z-20 rotate-[-45deg] bg-border">
-                          <div className="w-0 h-0 border-solid border-t-[20px] border-l-[20px] border-transparent border-l-muted rounded-sm"></div>
-                        </div>
-                      </div>
-                    )}
-                  </div>
-                </div>
-              )}
-              <Button
-                type="submit"
-                className="w-full"
-                disabled={
-                  newPassword &&
-                  requirements
-                    .map((req) => req.test(feedbackPassword))
-                    .some((r) => !r)
-                }
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(handleSubmit)}
+                className="grid gap-4"
               >
-                {t("loginButton")}
-              </Button>
-              <Button variant="outline" className="w-full">
-                {t("loginWithGoogle")}
-              </Button>
-            </form>
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>{t("emailLabel")}</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="email"
+                          placeholder={t("emailPlaceholder")}
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <div className="flex items-center">
+                        <FormLabel>{t("passwordLabel")}</FormLabel>
+                        <Link
+                          href="/forgot-password"
+                          className="ml-auto inline-block text-sm underline"
+                        >
+                          {t("forgotPassword")}
+                        </Link>
+                      </div>
+                      <FormControl>
+                        <Input type="password" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                      {newPasswordError && (
+                        <div className="text-red-500 text-sm">
+                          {newPasswordError}
+                        </div>
+                      )}
+                    </FormItem>
+                  )}
+                />
+
+                {newPassword && (
+                  <NewPasswordInput
+                    value={feedbackPassword}
+                    onChange={setFeedbackPassword}
+                    error={newPasswordError}
+                  />
+                )}
+
+                <Button
+                  type="submit"
+                  className="w-full"
+                  disabled={
+                    newPassword && !validateNewPassword(feedbackPassword)
+                  }
+                >
+                  {t("loginButton")}
+                </Button>
+                <Button variant="outline" className="w-full">
+                  {t("loginWithGoogle")}
+                </Button>
+              </form>
+            </Form>
           </CardContent>
         </Card>
       </div>
