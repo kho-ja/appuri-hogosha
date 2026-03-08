da8f797713dda6d614fa1a1f6d40069006adb749
Sunnatilla Sobitjonov <sunnatillasobidjonov@gmail.com>
2026-03-08 16:38:49 +0500

    returned localePrefix: "as-needed",

--- admin-panel-frontend/i18n/routing.ts
+++ admin-panel-frontend/i18n/routing.ts
@@ -3,7 +3,7 @@ import { defineRouting } from "next-intl/routing";
 export const routing = defineRouting({
   locales: ["en", "uz", "ja", "ru"],
   defaultLocale: "uz",
-  localePrefix: "always",
+  localePrefix: "as-needed",
 });
 
 export type Locale = (typeof routing.locales)[number];
