14b76d110831893be909c17c03dd9486d7c4e53a
Sunnatilla <sunnatillasobidjonov@gmail.com>
2026-03-29 22:41:59 +0500

    Disconnect vercel end fix otp bug (#600)
    deselected vercel

--- backend/src/utils/app.ts
+++ backend/src/utils/app.ts
@@ -6,9 +6,7 @@ import { config } from '../config';
 
 const allowedOrigins: (string | RegExp)[] = [
     config.FRONTEND_URL,
-    /^https:\/\/appuri-hogosha.*kho-jas-projects\.vercel\.app$/,
     /^https:\/\/.*\.d2hff850x2l8tj\.amplifyapp\.com$/,
-    /^https:\/\/appuri-hogosha\.vercel\.app$/,
 ];
 class App {
     public app: Application;
