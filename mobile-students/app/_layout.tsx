1ae6a6542e5df9436008ee90089cc722747e8e40
jdu211109 <211109m@jdu.uz>
2026-04-12 16:19:09 +0500

    feat: Implement mobile sign-in screen with email and Google login options

--- mobile-students/app/_layout.tsx
+++ mobile-students/app/_layout.tsx
@@ -6,7 +6,8 @@ import 'react-native-reanimated';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export const unstable_settings = {
-  anchor: '(tabs)',
+  initialRouteName: 'sign-in',
+  anchor: 'sign-in',
 };
 
 export default function RootLayout() {
@@ -15,6 +16,7 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
+        <Stack.Screen name="sign-in" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
